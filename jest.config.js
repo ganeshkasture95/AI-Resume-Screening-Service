ec719848f02494d5980b957c87cf4c08bb1055fb
Ganesh Kasture <ganeshkasture9595@gmail.com>
2026-04-14 16:06:38 +0530

    feat: setup backend dependencies, testing stack, and Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// const { createDefaultPreset } = require("ts-jest");\
+
+import { createDefaultPreset } from "ts-jest";
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
